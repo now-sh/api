272a604f2b440403764486c81eb5f202f2bdf2b7
Jason <git-admin@casjaysdev.com>
2022-01-10 15:56:55 -0500

    🗃 Modified: api/controllers/closings.js api/public/index.html  🗃

--- api/controllers/closings.js
+++ api/controllers/closings.js
@@ -2,6 +2,9 @@ const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
 const closingsurl = 'https://wnyt.com/closings/';
+let headers = [];
+let rows = [];
+let row = {};
 let cache = null;
 let lastCacheTime = null;
 
@@ -13,27 +16,8 @@ async function closings() {
   const html = await response.text();
   const $ = cheerio.load(html);
   const title = $('Closed').text();
-  const table = $('table ');
+  const table = $('table');
   const header = table.find('tbody tr');
-  const headers = [];
-  header.find('td').each((i, element) => {
-    headers.push($(element).text().trim().replace(/\W/g, '_'));
-  });
-  const rows = [];
-  $(table.find('tbody')[2])
-    .find('div.row')
-    .each((i, element) => {
-      const row = {};
-      $(element)
-        .find('')
-        .each((i, column) => {
-          row[headers[i]] = $(column).text().trim();
-          if (i !== 0) {
-            row[headers[i]] = String(row[headers[i]].remove() || 0);
-          }
-        });
-      rows.push(row);
-    });
   cache = rows;
   lastCacheTime = Date.now();
   return rows;
