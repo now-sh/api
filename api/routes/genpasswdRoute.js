5e33a3c603cf9658c949ef734d990035b8b2135c
Jason <git-admin@casjaysdev.com>
2022-02-08 22:36:48 -0500

    🗃️ modified: api/routes/genpasswdRoute.js 🗃️

--- api/routes/genpasswdRoute.js
+++ api/routes/genpasswdRoute.js
@@ -2,9 +2,11 @@ const express = require('express');
 const genpasswdRoute = express.Router();
 let totalPasswordsGenerated = 0;
 
+const title = 'Generate Passwords';
+
 const generatePassword = (length) => {
   const charset =
-    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=<>,./?|[]{};:`~';
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@%$';
   let password = '';
   for (let i = 0, n = charset.length; i < length; ++i) {
     password += charset.charAt(Math.floor(Math.random() * n));
@@ -12,17 +14,43 @@ const generatePassword = (length) => {
   return password;
 };
 
+genpasswdRoute.get('/', (req, res) => {
+  const length = 16;
+  const current = new Date();
+  const time = current.toLocaleTimeString('en-US');
+  totalPasswordsGenerated++;
+  res.json({
+    title: title,
+    Usage_Get: `curl -q -LSsf -X GET ${req.protocol}://${req.headers.host}/api/v1/passwd/10 | jq -r '.password'`,
+    Usage_Post: `curl -q -LSsf -X POST ${req.protocol}://${req.headers.host}/api/v1/passwd`,
+  });
+});
+
+genpasswdRoute.get('/:length', (req, res) => {
+  const length = req.params.length || 16;
+  const current = new Date();
+  const time = current.toLocaleTimeString('en-US');
+  totalPasswordsGenerated++;
+  res.json({
+    title: title,
+    password: generatePassword(length),
+    time: time,
+    passwordLength: length,
+    totalPasswordsGenerated: totalPasswordsGenerated,
+  });
+});
+
 genpasswdRoute.post('/', (req, res) => {
   const length = req.body.length || 16;
   const current = new Date();
   const time = current.toLocaleTimeString('en-US');
   totalPasswordsGenerated++;
   res.json({
-    title: 'Strong Password Generator::Generate',
+    title: title,
     password: generatePassword(length),
+    time: time,
     passwordLength: length,
     totalPasswordsGenerated: totalPasswordsGenerated,
-    time: time,
   });
 });
 
