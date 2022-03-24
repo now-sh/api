ceb6e2caaa4684623bb27ad3ce55fa29a271e0c3
Jason <git-admin@casjaysdev.com>
2022-03-23 21:43:46 -0400

    🗃️ modified: api/controllers/nys.js 🗃️

--- api/controllers/nys.js
+++ api/controllers/nys.js
@@ -6,7 +6,8 @@ const dtyester = datetime.create();
 dtyester.offsetInHours(-48);
 const yesterday = dtyester.format('Y-m-d');
 
-const nysurl = `https://health.data.ny.gov/resource/xdss-u53e.json?test_date=${yesterday}T00:00:00.000`;
+// const nysurl = `https://health.data.ny.gov/resource/xdss-u53e.json?test_date=${yesterday}T00:00:00.000`;
+const nysurl = 'https://health.data.ny.gov/resource/xdss-u53e.json';
 const cache = null;
 const lastCacheTime = null;
 
