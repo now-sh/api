c5381d0c07ab9486e33eb1ff3deda074885fc2aa
casjay <git-admin@casjaysdev.com>
2023-04-24 14:17:26 -0400

    🗃️  Modified: api/index.js 🗃️
    🗃️  Modified: api/index.js 🗃️

--- api/routes/redditRoute.js
+++ api/routes/redditRoute.js
@@ -4,27 +4,14 @@ const redditRoute = express.Router();
 const cors = require('cors');
 
 const fetch = require('node-fetch');
+const default_route = ['/', '/help'];
 
-redditRoute.get('/', cors(), async (req, res) => {
+redditRoute.get(default_route, cors(), async (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   try {
     res.send(
       JSON.stringify({
         Jason: `${req.protocol}://${req.headers.host}/api/v1/reddit/jason`,
-        Users: `${req.protocol}://${req.headers.host}/api/v1/reddit/u/:user`,
-        Reddits: `${req.protocol}://${req.headers.host}/api/v1/reddit/:subreddit`,
-      })
-    );
-  } catch (err) {
-    res.json({ error: err.message });
-  }
-});
-
-redditRoute.get('/:help', cors(), async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  try {
-    res.send(
-      JSON.stringify({
         Users: `${req.protocol}://${req.headers.host}/api/v1/reddit/u/:user`,
         Reddits: `${req.protocol}://${req.headers.host}/api/v1/reddit/:subreddit`,
       })
@@ -55,6 +42,25 @@ redditRoute.get('/jason', cors(), async (req, res) => {
 
 redditRoute.get('/u/:id', cors(), async (req, res) => {
   const response = await fetch(`https://www.reddit.com/u/${req.params.id}/.json?sort=new&limit=500`);
+  res.setHeader('Content-Type', 'application/json');
+  try {
+    const json = await response.json();
+    this.json = json.data.children;
+    this.json.shift;
+    this.json.shift;
+    this.json.shift;
+    res.send(
+      JSON.stringify({
+        reddit: this.json,
+      })
+    );
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+redditRoute.get('/r/:id', cors(), async (req, res) => {
+  const response = await fetch(`https://www.reddit.com/r/${req.params.id}/.json?sort=new&limit=500`);
   res.setHeader('Content-Type', 'application/json');
   try {
     const json = await response.json();
