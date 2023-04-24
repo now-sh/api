ef9b7c49f52f7350ffe714f3cb360d98157bc0fd
casjay <git-admin@casjaysdev.com>
2023-04-24 16:50:06 -0400

    🗃️  Modified: api/routes/commitRoute.js 🗃️
    🗃️  Modified: api/routes/healthRoute.js 🗃️

--- api/routes/healthRoute.js
+++ api/routes/healthRoute.js
@@ -5,13 +5,27 @@ const healthRoute = express.Router();
 healthRoute.get('/', cors(), async (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   try {
-    res.status(200).send(
-      JSON.stringify({
-        Status: 'Ok',
-      })
-    );
+    res.status(200).send(JSON.stringify({ Status: 'Ok' }));
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+healthRoute.get('/json', cors(), async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  try {
+    res.status(200).send(JSON.stringify({ Status: 'Ok' }));
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+healthRoute.get('/txt', cors(), async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  try {
+    res.status(200).send('Ok');
+  } catch (error) {
+    res.status(500).send({ error: error.message });
   }
 });
 
