866d4e2c60a3bdcd65ee3b132e37c9d6da86950b
casjay <git-admin@casjaysdev.com>
2023-04-24 16:50:01 -0400

    🗃️  Modified: api/routes/commitRoute.js 🗃️
    🗃️  Modified: api/routes/commitRoute.js 🗃️

--- api/routes/commitRoute.js
+++ api/routes/commitRoute.js
@@ -4,41 +4,57 @@ const path = require('path');
 const express = require('express');
 const commitRoute = express.Router();
 const cors = require('cors');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const messages = require('../lib/gitMessages');
 
-const commitment_url = process.env.GIT_MESSAGE_URL || 'https://commitment-6jyr.onrender.com';
+const url = process.env.GIT_MESSAGE_URL || 'https://commitment-6jyr.onrender.com';
 
-commitRoute.use(
-  '/',
-  createProxyMiddleware({
-    target: commitment_url,
-    changeOrigin: true,
-    pathRewrite: {
-      '^/api/v1/commit': '/',
-      '^/api/v1/commit/': '/',
-      '^/api/v1/commit/txt/': '/txt',
-      '^/api/v1/commit/text/': '/txt',
-      '^/api/v1/commit/json/': '/json',
-    },
-    onProxyRes: function (proxyRes, req, res) {
-      if (proxyRes.statusCode === 404) {
-        return res.json({ Error: 'Page not found.' });
-      }
-      if (proxyRes.statusCode === 500) {
-        return res.json({ Error: 'Something went horribly wrong.' });
-      }
-    },
-  })
-);
-
-commitRoute.get('/help', cors(), async (req, res) => {
-  if (cache && lastCacheTime > Date.now() - 1000 * 60 * 10) {
-    return cache;
-  }
+commitRoute.get('/', cors(), async (req, res) => {
+  const index = Math.floor(Math.random() * (messages.length + 1));
+  const commitMessage = `${messages[index]} `;
   res.setHeader('Content-Type', 'application/json');
   try {
     res.send(
       JSON.stringify({
+        message: commitMessage,
+      })
+    );
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+commitRoute.get('/json', cors(), async (req, res) => {
+  const index = Math.floor(Math.random() * (messages.length + 1));
+  const commitMessage = `${messages[index]} `;
+  res.setHeader('Content-Type', 'application/json');
+  try {
+    res.send(
+      JSON.stringify({
+        message: commitMessage,
+      })
+    );
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+commitRoute.get('/txt', cors(), async (req, res) => {
+  const index = Math.floor(Math.random() * (messages.length + 1));
+  const commitMessage = `${messages[index]} `;
+  res.setHeader('Content-Type', 'text/plain');
+  try {
+    res.send(commitMessage);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+commitRoute.get('/help', cors(), async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  try {
+    res.send(
+      JSON.stringify({
+        url: url,
         default: `${req.protocol}://${req.headers.host}/api/v1/commit/`,
         txt: `${req.protocol}://${req.headers.host}/api/v1/commit/txt/`,
         json: `${req.protocol}://${req.headers.host}/api/v1/commit/json/`,
