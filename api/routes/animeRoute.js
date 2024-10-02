0c150237a7bbc84e05c11ba15bd87f6cac304c4e
casjay <git-admin@casjaysdev.pro>
2024-10-02 14:13:12 -0400

    🗃️ Committing everything that changed 🗃️
    api/index.js
    api/public/index.html
    api/routes/animeRoute.js
    api/routes/defaultRoute.js
    api/routes/mainRoute.js

---
+++ api/routes/animeRoute.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require('express');
+const animeRoute = express.Router();
+const cors = require('cors');
+
+const fetch = require('node-fetch');
+const default_route = ['/', '/help'];
+
+animeRoute.get(default_route, cors(), async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  try {
+    res.send(
+      JSON.stringify({
+        quote: `${req.protocol}://${req.headers.host}/api/v1/anime/quote`,
+      })
+    );
+  } catch (err) {
+    res.json({ error: err.message });
+  }
+});
+
+animeRoute.get('/quote', cors(), async (req, res) => {
+  const response = await fetch(`https://animechan.io/api/v1/quotes/random`, {
+    method: "GET",
+    mode: 'no-cors'
+  });
+  res.setHeader('Content-Type', 'application/json');
+  try {
+    const json = await response.json();
+    this.json = json.data;
+    res.send(
+      JSON.stringify({
+        quote: this.json.content,
+        anime: this.json.anime.name,
+        character: this.json.character.name,
+      })
+    );
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+module.exports = animeRoute;
